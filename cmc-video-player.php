8d8097179c6dbb8607d9d9f5f9875399bda0e00b
Michael Wender <michael@michaelwender.com>
2024-01-09 09:51:07 -0500

    First commit

---
+++ cmc-video-player.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     CMC Video Player
+ * Plugin URI:      https://github.com/WenderHost/cmc-video-player
+ * Description:     Plugin for adding Cloud Media Center's video player to your WordPress site. Provides the <code>[cmcplayer/]</code> shortcode.
+ * Author:          Michael Wender
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     cmc-video-player
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Cmc_Video_Player
+ */
+
+// Your code starts here.
